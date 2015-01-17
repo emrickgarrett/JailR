e9022027669be98b2e84b3a24c254962fd565f86
Garrett <emrickgarrett@gmail.com>
2015-01-17 05:47:46 -0500

    Have a nap Mel

--- background.js
+++ background.js
@@ -43,8 +43,46 @@ function savetext(info,tab) {
 
 function processResults(result){
 
+    if(result.status == 1){
+
+
+        if(result.records.length == 0){
+            alert("No Record");
+            return;
+        }
+
+        popup(result);
+
+
+
+    }else{
+        alert(result.msg);
+    }
     //Display it somewhere in the browser...
 
+}
+
+function popup(result)
+{
+    var generator=window.open('','name','height=800,width=1000');
+
+    generator.document.write('<html><head><title>Popup</title>');
+    generator.document.write("<link rel='stylesheet' href='mystyle.css'></head><body>");
+
+    generator.document.write("<table class='result-table'");
+    for(var i = 0; i < result.records.length; i++){
+        //alert(result.records[i].name);
+        generator.document.write("<tr>");
+        generator.document.write("<td><img class='mugshot'  src='" + result.records[i].mugshot + "'/></td>");
+        generator.document.write("<td><span class='name'>" + result.records[i].name + "</span></td>");
+        generator.document.write("<td><span class='county-state'>" + result.records[i].county_state +"</span></td>");
+        generator.document.write("<td><span class='book-date'>" + result.records[i].book_date +"</span></td>");
+        generator.document.write("<td><span class='charges'>" + result.records[i].charges + "</span></td>");
+        generator.document.write("</tr>");
+    }
+    generator.document.write("</table>");
+    generator.document.write('</body></html>');
+    generator.document.close();
 }
 
 function confirmResults(firstName, lastName){
